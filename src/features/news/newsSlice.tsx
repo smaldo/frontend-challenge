b5f3da2468311c596ff182643ed74c8cb4d34baf
smaldo <sergiomaldonadoolguin@gmail.com>
2022-02-02 16:58:51 -0300

    State structure update, creation of API call thunk, creation of new reducers, creation of selectors

--- src/features/news/newsSlice.tsx
+++ src/features/news/newsSlice.tsx
@@ -1,48 +1,79 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState, AppThunk } from '../../app/store';
+import { RootState, AppThunk, AppThunkDispatch } from '../../app/store';
 import { INewsItem } from './types';
+import axios from 'axios';
 
 export interface INewsState {
-    news: INewsItem[];
+    news: { [key: string]: INewsItem };
     favs: {
-        'Reactjs': INewsItem[],
-        'Angular': INewsItem[],
-        'Vuejs': INewsItem[],
+        'Reactjs': { [key: string]: INewsItem };
+        'Angular': { [key: string]: INewsItem };
+        'Vuejs': { [key: string]: INewsItem };
     };
     show: 'all' | 'favs';
     query: 'Reactjs' | 'Angular' | 'Vuejs';
     page: number;
 }
-
 const initialState: INewsState = {
-    news: [],
+    news: {},
     favs: {
-        'Reactjs': [],
-        'Angular': [],
-        'Vuejs': [],
+        'Reactjs': {},
+        'Angular': {},
+        'Vuejs': {},
     },
     show: 'all',
     query: 'Reactjs',
     page: 1,
 };
 
+export const NewsAPICall = createAsyncThunk(
+    'news/fetchNews',
+    async ({currentQuery, currentPage}: {currentQuery: string, currentPage: number}) => {
+        const response = await axios.get(`https://hn.algolia.com/api/v1/search_by_date?query=${currentQuery}&page=${currentPage}`);
+        const news: { [key: string]: INewsItem } = {};
+        response.data.hits.map( ({ objectID, author, created_at, story_title, story_url }: INewsItem) => {
+            news[objectID] = { objectID, author, created_at, story_title, story_url };
+            return { objectID, author, created_at, story_title, story_url };
+        });
+        return news;
+    }
+  );
 
 export const newsSlice = createSlice({
     name: 'news',
     initialState,
     reducers: {
+        addFav: (state, action: PayloadAction<INewsItem>) => {
+            state.favs[state.query][action.payload.objectID] = action.payload;
+        },
+        deleteFav: (state, action: PayloadAction<string>) => {
+            delete state.favs[state.query][action.payload];
+        },
         changeShow: (state, action: PayloadAction<'all' | 'favs'>) => {
             state.show = action.payload;
         },
-        selectQuery: (state, action: PayloadAction<'Reactjs' | 'Angular' | 'Vuejs'>) => {
+        changeQuery: (state, action: PayloadAction<'Reactjs' | 'Angular' | 'Vuejs'>) => {
             state.query = action.payload;
+            state.page = 1;
         },
-        nextPage: (state) => {
-            state.page += 1;
+        nextPage: (state, action: PayloadAction<number>) => {
+            state.page = action.payload;
         },
+    },
+    extraReducers: (builder) => {
+        builder
+          .addCase(NewsAPICall.fulfilled, (state, action) => {
+            state.news = action.payload;
+          });
     },
 });
 
-export const { changeShow, selectQuery, nextPage } = newsSlice.actions;
+export const { addFav, deleteFav, changeShow, changeQuery, nextPage } = newsSlice.actions;
+
+export const selectNews = (state: RootState) => state.news.news;
+export const selectFavs = (state: RootState) => state.news.favs;
+export const selectShow = (state: RootState) => state.news.show;
+export const selectQuery = (state: RootState) => state.news.query;
+export const selectPage = (state: RootState) => state.news.page;
 
 export default newsSlice.reducer;
